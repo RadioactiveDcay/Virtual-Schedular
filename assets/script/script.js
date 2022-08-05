ff9364ea6b45c54c38e32d537c538ece629ccba1
RadioactiveDcay <33047670+RadioactiveDcay@users.noreply.github.com>
2022-08-04 21:57:10 -0700

    update: added click input for the list. and a delete btn for so the items can be removed.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,13 @@
 var list;
 var input;
-var inputData;
 var delInput;
-var container;
 
 function editlist (edit) {
     let input = edit.target.innerHTML;
     let itemInput = document.createElement('input');
     itemInput.type = 'text';
     itemInput.value = item;
-    itemInput.classList.add('keypress', saveItem);
+    itemInput.classList.editableList('keypress', saveItem);
     itemInput.addEventListener('click', saveItem);
     edit.target.parentNode.prepend(itemInput);
     edit.target.remove();
@@ -24,3 +22,16 @@ function input (click_1) {
     editList.addEventListener;
 }
 
+function saveBtn () {
+    var saveBtn;
+    let btn = click;
+}
+
+function delete_data (delInput) {
+    $ Delete_Btn (remove.itemInput)}
+    {
+    let input = addEventListener.remove.input;
+    itemInput.type = 'text';
+    itemInput.value = item;
+    itemInput.classList.delete_data
+}
